c750615a2614ed5c81d22444361cdeef2224eba0
donnerbart <github@thunderphreak.de>
2017-02-07 12:56:33 +0100

    Added number of invocations and avg duration per invocation to TimeTracker

--- src/main/java/com/hazelcast/qasonar/utils/TimeTracker.java
+++ src/main/java/com/hazelcast/qasonar/utils/TimeTracker.java
@@ -29,31 +29,39 @@ import static java.util.concurrent.TimeUnit.NANOSECONDS;
 public final class TimeTracker {
 
     private static final float PERCENTAGE = 100f;
-    private static final ConcurrentMap<TimeTrackerLabel, Long> TIME_MAP = new ConcurrentHashMap<>();
+    private static final ConcurrentMap<TimeTrackerLabel, Long> DURATIONS = new ConcurrentHashMap<>();
+    private static final ConcurrentMap<TimeTrackerLabel, Long> INVOCATIONS = new ConcurrentHashMap<>();
 
     private TimeTracker() {
     }
 
     public static void record(TimeTrackerLabel label, long elapsedNanos) {
-        TIME_MAP.compute(label, (s, oldValue) -> oldValue == null ? elapsedNanos : oldValue + elapsedNanos);
+        DURATIONS.compute(label, (s, oldDuration) -> oldDuration == null ? elapsedNanos : oldDuration + elapsedNanos);
+        INVOCATIONS.compute(label, (s, oldInvocations) -> oldInvocations == null ? 1 : oldInvocations + 1);
     }
 
     public static void printTimeTracks() {
-        long totalTime = 0;
-        for (Map.Entry<TimeTrackerLabel, Long> entry : TIME_MAP.entrySet()) {
-            totalTime += entry.getValue();
+        long totalDuration = 0;
+        long totalInvocations = 0;
+        for (Map.Entry<TimeTrackerLabel, Long> entry : DURATIONS.entrySet()) {
+            totalDuration += entry.getValue();
+            totalInvocations += INVOCATIONS.get(entry.getKey());
         }
-        if (totalTime == 0) {
+        if (totalDuration == 0 || totalInvocations == 0) {
             return;
         }
 
         print("\nTimeTracker statistics");
-        for (Map.Entry<TimeTrackerLabel, Long> entry : sortByValue(TIME_MAP).entrySet()) {
+        for (Map.Entry<TimeTrackerLabel, Long> entry : sortByValue(DURATIONS).entrySet()) {
+            TimeTrackerLabel label = entry.getKey();
             long duration = entry.getValue();
-            float percentage = PERCENTAGE * duration / totalTime;
-            print("%s: %d ms (%.2f%%)", entry.getKey(), NANOSECONDS.toMillis(duration), percentage);
+            long invocations = INVOCATIONS.get(label);
+            float percentage = PERCENTAGE * duration / totalDuration;
+            long average = (long) (duration / (double) invocations);
+            print("%s: %d ms (%.2f%%) (%d invocations) (%d ms per invocation)", label, NANOSECONDS.toMillis(duration), percentage,
+                    invocations, NANOSECONDS.toMillis(average));
         }
-        print("Total time: %d ms (%.2f%%)%n", NANOSECONDS.toMillis(totalTime), PERCENTAGE);
+        print("Total time: %d ms (%.2f%%) (%d invocations)%n", NANOSECONDS.toMillis(totalDuration), PERCENTAGE, totalInvocations);
     }
 
     private static <K, V extends Comparable<? super V>> Map<K, V> sortByValue(Map<K, V> map) {
